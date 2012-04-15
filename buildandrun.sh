4a05ef3d12880c7ae444996e1e9b1c88efdf4251
Dan McArdle <zingermc@gmail.com>
2012-04-14 23:45:07 -0400

    added generality to compile script

--- buildandrun.sh
+++ buildandrun.sh
@@ -3,5 +3,5 @@
 # Compiles project for OSX and passes arguments specified to this script on.
 #
 
-gcc -framework GLUT -framework OpenGL -framework Cocoa -o checkers.exe checkers.c && \
+gcc -framework GLUT -framework OpenGL -framework Cocoa -o checkers.exe *.c && \
 ./checkers.exe $*
