c78f0467f96f26edf828728c1006a79a3c1a4eb0
AndrewGeneseo <andrew.leung.adl@gmail.com>
2012-04-30 20:08:05 -0300

    Update buildandrun.sh

--- buildandrun.sh
+++ buildandrun.sh
@@ -3,5 +3,6 @@
 # Compiles project for OSX and passes arguments specified to this script on.
 #
 
+killall checkers.exe && \
 gcc -framework GLUT -framework OpenGL -framework Cocoa -o checkers.exe *.c && \
 ./checkers.exe $*
